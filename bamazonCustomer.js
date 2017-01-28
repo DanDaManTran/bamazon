593ccb046e6c178d394ca44a4fae366193e5029e
DanDaManTran <DannDoesntKnow@dans-mbp.home>
2017-01-28 00:56:11 -0600

    finish the Customer.JS -DT

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,19 +21,41 @@ connection.connect(function(err) {
   if (err) throw err;
 });
 
-//displaying the list a customer can purchase from the data base 
-connection.query("SELECT * FROM products", function(err, res) {
-	if(err) throw err;
-
-  for(var i=0; i<res.length; i++){
-    console.log("ID: " + res[i].item_id + " | " + "Item: " + res[i].product_name + " | " + "Price: " + res[i].price);
+//adding spacing to the string so it can create a table in the console
+function idString(id){
+  let idInput = "ID: "+ id;
+  while(idInput.length<6){
+    idInput = idInput + " ";
   }
 
-  connection.end();
-  posting(question());
-});
+  return idInput;
+};
 
+//adding spacing to the string so it can create a table in the console
+function nameString(name){
+  let nameInput = "Item: " + name;
+  while(nameInput.length<40){
+    nameInput = nameInput + " ";
+  }
 
+  return nameInput;
+};
+
+//displaying the list a customer can purchase from the data base 
+function display () {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if(err) throw err;
+
+    for(var i=0; i<res.length; i++){
+      console.log(idString(res[i].item_id) + " |  " + nameString(res[i].product_name) + "|  " + "Price: " + res[i].price.toFixed(2));
+      console.log("-----------------------------------------------------------------------------");
+    }
+
+    question();
+  });
+};
+
+//function to ask buyers questions and then sending it into posting function
 function question(){
   inquirer.prompt([
     {
@@ -69,14 +91,47 @@ function question(){
       }
     }
   ]).then(function(user) {
-    return user.itemID + ", " + user.quantity;
+
+    checking(user.itemID, user.quantity);
+
   });
 };
 
-function posting(ID, quant){
-  if(ID != undefined || quant != undefined) {
-    console.log(ID + " " + quant);
-    console.log("hello");
-  }
+//it will check if there are enough in the inventory before it do anything else. then it would console.log the status and update purchase when needed
+function checking (ID, quant){
+    connection.query("SELECT stock_quantity, product_name, price FROM products WHERE ?",{item_id: ID} , function(err, res){
+      if(err) throw err;
+
+      var newStock = res[0].stock_quantity - quant;
+
+      if(res[0].stock_quantity<quant){
+        console.log("Insufficient quantity!");
+        connection.end();
+      } else if(res[0].stock_quantity===parseInt(quant)){
+        console.log("Lucky You. You just bought the last " + res[0].product_name + ". Have Fun!");
+        updatePurchase(ID, 0);
+        total(res[0].price, quant);
+        connection.end();
+      } else if(res[0].stock_quantity>quant){
+        console.log("Nice purchase! " + res[0].product_name + " is super fun!");
+        updatePurchase(ID, newStock);
+        total(res[0].price, quant);
+        connection.end();
+      }
+    });
 };
+
+//it will caculate the total price
+function total(price, count){
+  console.log("Your total is " + price*count);
+};
+
+//it will update the DB after the purchase
+function updatePurchase(ID, quant){
+  connection.query("UPDATE products SET ? WHERE ?", [{stock_quantity: quant}, {item_id: ID}], function(err, res) {});
+};
+
+//starting running the app
+display();
+
 
